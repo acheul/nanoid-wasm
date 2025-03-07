4156deea12a93704c168263beda1bf41bd9c1f77
Acheul <seoul605.21@gmail.com>
2025-03-07 21:44:58 +0900

    v0.1.1

--- examples/wasm-leptos/src/main.rs
+++ examples/wasm-leptos/src/main.rs
@@ -10,30 +10,28 @@ pub fn main() {
 pub fn App() -> impl IntoView {
 
   let (size, set_size) = signal(String::from("21"));
-  let size_ = Memo::new(move |_| {
-    if let Ok(size) = size.get().parse::<u32>() {
-      size
-    } else {
-      21
-    }
-  });
+  let size_ = Memo::new(move |_| size.get().parse::<u32>().unwrap_or(21).min(100).max(2));
 
   let (ids, set_ids) = signal(Vec::<String>::new());
 
+  // create nanoids
   let create_ids = move |_| {
-    let ids = (0..10).map(|_| nanoid_wasm::nanoid(size_.get())).collect::<Vec<_>>();
+    let ids = (0..10).map(|_| nanoid_wasm::nanoid!(size_.get())).collect::<Vec<_>>();
     set_ids.set(ids);
   };
 
   view! {
     <div>
-      <span>"size: "</span>
-      <input 
-        type="text"
-        bind:value=(size, set_size)
-      />
-      <br/>
-      <button on:click=create_ids>"Create some Ids with size " {move || size_.get()}</button>
+      <div>
+        <span>"size(2~100):  "</span>
+        <input 
+          type="text"
+          style="width: 80px"
+          bind:value=(size, set_size)
+        />
+        <br/>
+        <button on:click=create_ids style="font-weight: bold;">"Create Random Ids with size " {move || size_.get()}</button>
+      </div>
       <div>
         <For
           each=move || ids.get()
